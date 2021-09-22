c16beb595b664dd210826c4c90ce7ec79d64ce42
DomerKek <vladyslav.postrelko@gmail.com>
2021-09-22 10:04:54 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Management;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Info i = new Info();
+
+            string text = i.GetOSInfo() + "\n" + i.GetComputerName() + "\n" + i.GetTimeZone() + "\n" + i.GetNETversion();
+            try {
+
+
+                TcpClient client = new TcpClient("127.0.0.1", 4444);
+               
+                Console.WriteLine("Connected!");
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.Unicode.GetBytes(text);
+                
+               
+                Console.WriteLine("Sending : " + text);
+                stream.Write(data, 0, data.Length);
+
+                // закрываем сокет
+                client.Close();
+                Console.Read();
+                
+            }
+           
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                Console.Read();
+            }
+        } 
+    }
+    public class Info
+    {
+        public string GetOSInfo()
+        {
+            string result = "";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                result = os["Caption"].ToString();
+                break;
+            }
+            return result;
+        }
+        public string GetComputerName()
+        {
+            return "Computer name: " + Environment.MachineName;
+        }
+        public string GetTimeZone()
+        {
+            return "Current Time Zone: " + TimeZoneInfo.Local.DisplayName;
+        }
+        public string GetNETversion()
+        {
+            return ".NET version: " + Environment.Version;
+        }
+    }
+}
---
+++ TestTask1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Management;
+using System.Net;
+using System.Net.Sockets;
+//using WebSocketSharp;
+//using WebSocketSharp.Server;
+
+namespace TZIDR
+{
+    class ClientServer
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 4444);
+
+                server.Start();
+                Console.WriteLine("Server has started on 127.0.0.1:4444.");
+                Console.WriteLine("The local End point is  :" + server.LocalEndpoint);
+                Console.WriteLine("Waiting for new connection...");
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("A client connected.");
+
+                    NetworkStream stream = client.GetStream();
+
+                    byte[] buffer = new byte[client.ReceiveBufferSize];
+
+                    //---read incoming stream---
+                    int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                    //---convert the data received into a string---
+                    string dataReceived = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Received : " + dataReceived);
+
+
+
+
+
+                    // закрываем сокет
+                    stream.Close();
+                    client.Close();
+                }
+                server.Stop();
+                Console.ReadLine();
+
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+
+        }
+    }
+}
